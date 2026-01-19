19af8230140ed9ac24739d2e99ba2f2869f19575
cauan745 <cauanramos745@gmail.com>
2026-01-19 19:12:06 -0400

    Added first endpoint and guarantee server will run

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
 app.listen(port, ()=>{
     console.log("Server Running on Port:" + port)
 })
