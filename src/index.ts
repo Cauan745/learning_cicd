ceea00d2196275fe908d38b3a3491fe3f40e4d86
cauan745 <cauanramos745@gmail.com>
2026-01-19 19:23:25 -0400

    Added another endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,18 @@ const port = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello world!")
+    res.json({
+        message: "Hello world!"
+    })
+})
+
+app.get("/:name", (req, res) => {
+    const name = req.params.name
+    
+    res.json({
+        message: `Hello ${name}!`,
+        name
+    })
 })
 
 app.listen(port, ()=>{
